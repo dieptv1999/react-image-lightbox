d9f819492d6dd7b2a19e1cc3bb226c329fb730c7
dieptv <diep.tv1999@gmail.com>
2024-12-29 11:35:29 +0700

    init

--- test-config/test-setup.js
+++ test-config/test-setup.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import 'regenerator-runtime/runtime';
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import Adapter from '@cfaester/enzyme-adapter-react-18';
 
 configure({ adapter: new Adapter() });
